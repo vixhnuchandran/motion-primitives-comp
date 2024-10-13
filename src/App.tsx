7c0924f623a9696b9168ce296ae960457fdb9fa7
vixhnuchandran <vixhnuchandran@gmail.com>
2024-10-14 04:14:30 +0530

    Add inview comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from "react";
-// import Accordion from "@/components/samples/accordion";
+import React, { useRef } from "react";
 import AnimatedBackground from "@/components/samples/animatedBackground";
 import AnimatedGroup from "@/components/samples/animatedGroup";
 import AnimatedNumber from "@/components/samples/animatedNumber";
-// import { BorderTrail } from "@/components/samples/borderTrail";
-// import { Carousel } from "@/components/samples/carousel";
 import Cursor from "@/components/samples/cursor";
 import Dialog from "@/components/samples/dialog";
-// import { Disclosure } from "@/components/samples/disclosure";
 import Dock from "@/components/samples/dock";
 import InfiniteSlider from "@/components/samples/infiniteSlider";
-// import { InView } from "@/components/samples/inView";
 import Popover from "@/components/samples/popover";
-// import { ScrollProgress } from "@/components/samples/scrollProgress";
 import TextEffect from "@/components/samples/textEffect";
-import { ThemeProvider } from "@/components/theme/themeProvider";
-// import { InView } from "./components/ui/in-view";
+// import Accordion from "@/components/samples/accordion";
+// import { Disclosure } from "@/components/samples/disclosure";
+// import { BorderTrail } from "@/components/samples/borderTrail";
+// import { Carousel } from "@/components/samples/carousel";
+// import { ScrollProgress } from "@/components/samples/scrollProgress";
+import { InView } from "./components/ui/in-view";
 // import ToolbarDynamic from "@/components/samples/toolbarDynamic";
 // import { ToolbarExpandable } from "@/components/samples/toolbarExpandable";
 // import { TransitionPanel } from "@/components/samples/transitionPanel";
-import ThemeSwitch from "./components/theme/themeSwitch";
 
 const components = [
   "Animated Group",
@@ -65,19 +62,32 @@ const componentMap: { [key: string]: React.FC } = {
 };
 
 const App: React.FC = () => {
-  return (
-    <ThemeProvider>
-      <div className="flex flex-col min-h-screen bg-white">
-        <div className="flex  justify-center dark:text-white">
-          <ThemeSwitch />
-        </div>
-        <main className="flex-grow p-4 flex items-center justify-center ">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6">
-            {components.map((componentName, index) => {
-              const camelCaseName = componentName.replace(/\s+/g, "");
-              const Component = componentMap[camelCaseName];
+  const containerRef = useRef<HTMLDivElement>(null);
 
-              return (
+  return (
+    <div className="flex flex-col min-h-screen bg-white" ref={containerRef}>
+      <div className="pointer-events-none absolute left-0 top-0 w-full"></div>
+      <main className="flex-grow p-4 flex items-center justify-center ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6">
+          {components.map((componentName, index) => {
+            const camelCaseName = componentName.replace(/\s+/g, "");
+            const Component = componentMap[camelCaseName];
+
+            return (
+              <InView
+                viewOptions={{ once: true, margin: "0px 0px -250px 0px" }}
+                variants={{
+                  hidden: {
+                    opacity: 0,
+                  },
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      staggerChildren: 0.09,
+                    },
+                  },
+                }}
+              >
                 <div
                   key={index}
                   className="bg-gray-200 shadow-md rounded-lg p-4 w-[600px] h-[400px] flex flex-col items-center justify-center"
@@ -89,12 +99,12 @@ const App: React.FC = () => {
                     {Component ? <Component /> : <p>Component not found</p>}
                   </div>
                 </div>
-              );
-            })}
-          </div>
-        </main>
-      </div>
-    </ThemeProvider>
+              </InView>
+            );
+          })}
+        </div>
+      </main>
+    </div>
   );
 };
 
